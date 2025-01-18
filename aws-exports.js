b7cda16a7289cbbb0682ec7ba2792f67065d83d9
ca11_911 <zetxc996@gmail.com>
2025-01-18 20:07:31 +0600

    chore: initial setup commit

--- aws-exports.js
+++
@@ -1,8 +0,0 @@
-export default {
-  Auth: {
-    region: 'your-region',
-    userPoolId: 'your-user-pool-id',
-    userPoolWebClientId: 'your-app-client-id',
-    authenticationFlowType: 'USER_PASSWORD_AUTH', // Optional: defines the auth flow
-  },
-};
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,25 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Plugins
 import { registerPlugins } from '@/plugins'
 
 // Components
 import App from './App.vue'
-
-// Composables
 import { createApp } from 'vue'
+import { Amplify } from 'aws-amplify'
+
+const awsConfig = {
+    region: import.meta.env["VITE_AWS_REGION"] as string,
+    userPoolId: import.meta.env["VITE_USER_POOL_ID"] as string,
+    userPoolWebClientId: import.meta.env["VITE_USER_POOL_WEB_CLIENT_ID"] as string,
+}
+
+Amplify.configure({
+  auth: {
+    aws_region: awsConfig.region,
+    user_pool_id: awsConfig.userPoolId,
+    user_pool_client_id: awsConfig.userPoolWebClientId,
+    authentication_flow_type: "USER_SRP_AUTH"
+  }
+});
+
 
 const app = createApp(App)
 
