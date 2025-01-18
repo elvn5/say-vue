c7c65b1725a3bf4242479558056899a9db301b78
ca11_911 <zetxc996@gmail.com>
2025-01-18 19:23:11 +0600

    chore: initial commit

---
+++ aws-exports.js
@@ -0,0 +1,8 @@
+export default {
+  Auth: {
+    region: 'your-region',
+    userPoolId: 'your-user-pool-id',
+    userPoolWebClientId: 'your-app-client-id',
+    authenticationFlowType: 'USER_PASSWORD_AUTH', // Optional: defines the auth flow
+  },
+};
