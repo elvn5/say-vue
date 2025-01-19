1281395cbd45983cebd0720b5f20898c62c1592d
ca11_911 <zetxc996@gmail.com>
2025-01-19 14:55:27 +0600

    chore: manual setup of ampify

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,9 @@ import App from './App.vue'
 import { createApp } from 'vue'
 import { Amplify } from 'aws-amplify'
 
-const awsConfig = {
-    region: import.meta.env["VITE_AWS_REGION"] as string,
-    userPoolId: import.meta.env["VITE_USER_POOL_ID"] as string,
-    userPoolWebClientId: import.meta.env["VITE_USER_POOL_WEB_CLIENT_ID"] as string,
-}
+import amplfyOutputs from "../amplify_outputs.json"
 
-Amplify.configure({
-  auth: {
-    aws_region: awsConfig.region,
-    user_pool_id: awsConfig.userPoolId,
-    user_pool_client_id: awsConfig.userPoolWebClientId,
-    authentication_flow_type: "USER_SRP_AUTH"
-  }
-});
+Amplify.configure(amplfyOutputs);
 
 
 const app = createApp(App)
